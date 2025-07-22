e221ddded58215099cde66e8de9acd203ff440fa
Sven <svanhuessen@gmail.com>
2025-07-22 19:35:42 +0200

    feat: better example

--- solution/GenericDispatcher/Main.cpp
+++ solution/GenericDispatcher/Main.cpp
@@ -2,78 +2,94 @@
 #include <string>
 #include <iostream>
 
-using json = std::string; // Placeholder for JSON type
-
-template<typename T, typename...Args>
-struct serialize_impl {
-	//static json Run(const T&, Args&&...);
-};
-
-template<typename T, typename...Args>
-struct visitable_serialize_impl {
-	//static json Run(const T&, Args&&...);
-};
-
-template<typename T, typename...Args>
-struct default_serialize_impl {
-	//static json Run(const T&, Args&&...);
-};
-
-
-using JsonSerializerDispatcher =
-svh::AnyDispatcher<
-	serialize_impl,
-	visitable_serialize_impl,
-	default_serialize_impl
->;
-
-struct Serializer {
+/* My library */
+namespace MyLogger {
 	template<typename T, typename...Args>
-	static json ToJson(const T& v, Args&&... args) {
-		return JsonSerializerDispatcher::Run(
-			v, std::forward<Args>(args)...);
+	struct user_impl {};
+
+	template<typename T, typename...Args>
+	struct library_impl {};
+
+	template<typename T, typename...Args>
+	struct fallback_impl {
+		static std::string Run(const T& value, Args&&... args) {
+			return "Could not convert.";
+			/* Or */
+			//static_assert(svh::always_false<T>::value, "No matching provider for this type/args!");
+		}
+	};
+
+	using LoggerDispatcher = svh::AnyDispatcher<
+		user_impl, /* First check if the user has implemented it */
+		library_impl, /* Then check if the library has an implementation */
+		fallback_impl /* Else, use a fallback implementation */
+	>;
+
+	template<typename T, typename... Args>
+	static std::string Log(const T& value, Args&&... args) {
+		return LoggerDispatcher::Run(value, std::forward<Args>(args)...);
 	}
+
+
+	/* Library implementations */
+	template<>
+	struct library_impl<int> {
+		static std::string Run(const int& value) {
+			return std::to_string(value);
+		}
+	};
+
+	template<>
+	struct library_impl<bool> {
+		static std::string Run(const bool& value) {
+			return value ? "true" : "false";
+		}
+	};
+}
+
+/* User decides they don't like the way bools are logged */
+/* They can just "overwrite" it */
+namespace MyLogger {
+	template<>
+	struct user_impl<bool> {
+		static std::string Run(const bool& value) {
+			return value ? "yes" : "no";
+		}
+	};
+}
+
+struct MyStruct {
+	int a;
+	bool b;
+	float c;
 };
 
-#define SERIALIZE_IMPL 1
-
-#if SERIALIZE_IMPL == 1
-
-template<>
-struct serialize_impl<int, bool> {
-	static json Run(const int& v, const bool test) {
-		return test ? std::to_string(v) :
-			"{\"int\": " + std::to_string(v) + "}";
-	}
-};
-
-//#elif SERIALIZE_IMPL == 2
-
-template<>
-struct visitable_serialize_impl<int, bool> {
-	static json Run(const int& v, const bool test) {
-		return "{\"value\": " + std::to_string(v) + "}";
-	}
-};
-
-#elif SERIALIZE_IMPL == 3
-
-template<>
-struct default_serialize_impl<int> {
-	static json Run(const int& v) {
-		return "{\"int\": " + std::to_string(v) + "}";
-	}
-};
-#else
-
-#error "Please define SERIALIZE_IMPL to choose a serialization implementation."
-
-#endif
+/* User can also implement their own types */
+namespace MyLogger {
+	template<>
+	struct user_impl<MyStruct> {
+		static std::string Run(const MyStruct& value) {
+			/* Convert each member to string (either using the same library or some other way) */
+			auto a_str = MyLogger::Log(value.a); /* This will use library_impl<int> */
+			auto b_str = MyLogger::Log(value.b); /* This will use user_impl<bool> */
+			auto c_str = MyLogger::Log(value.c); /* This will use fallback_impl<float> */
+			return "MyStruct(" + a_str + ", " + b_str + ", " + c_str + ")";
+		}
+	};
+}
 
 int main() {
 	int value = 42;
-	std::cout << "Serialized JSON: " << Serializer::ToJson(value, true) << std::endl;
-	std::cout << "Serialized JSON: " << Serializer::ToJson(value, false) << std::endl;
+	std::cout << "my value: " << MyLogger::Log(value) << std::endl; // Will use library_impl<int>
+
+	bool flag = true;
+	std::cout << "my flag: " << MyLogger::Log(flag) << std::endl; // Will use user_impl<bool>
+
+	float floatValue = 3.14f;
+	std::cout << "my float value: " << MyLogger::Log(floatValue) << std::endl; // Will use fallback_impl<float>
+
+	MyStruct myStruct{ 1, false, 2.718f };
+	std::cout << "my struct: " << MyLogger::Log(myStruct) << std::endl; // Will use user_impl<MyStruct>
 
 	//wait for user input before exiting
 	std::cout << "Press Enter to exit...";
